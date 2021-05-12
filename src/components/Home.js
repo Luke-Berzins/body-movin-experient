37c30051171f3be68e25fe4f43d40f3cdd59cd39
Luke-Berzins <lukeberzins16@gmail.com>
2021-05-12 12:32:01 -0400

    preparing to import lottie

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,7 @@ export default function Home(props) {
         <div>
             <p>
                 Hello!
+
             </p>
         </div>
     )
--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -1,28 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 
 export default function NavBar(props) {
+    const [doctitle, setDocTitle] = useState("Lizzy's")
 
+    useEffect(() => {
+        document.title = doctitle
+    }, [doctitle])
 
 
     return (
         <nav>
             <ul>
                 <li>
-                    <Link to="/">Home</Link>
+                    <Link 
+                        to="/" 
+                        onClick={() => setDocTitle("Home")}
+                    >Home</Link>
                 </li>
                 <li>
-                    <Link to="/about">About</Link>
+                    <Link 
+                        to="/about"
+                        onClick={() => setDocTitle("About")}
+                    >About</Link>
                 </li>
                 <li>
-                    <Link to="/blog">Blog</Link>
+                    <Link 
+                        to="/blog"
+                        onClick={() => setDocTitle("Blog")}
+                    >Blog</Link>
                 </li>
                 <li>
-                    <Link to="/contact">Contact</Link>
+                    <Link 
+                        to="/contact"
+                        onClick={() => setDocTitle("Contact me!")}
+                    >Contact</Link>
                 </li>
                 <li>
-                    <Link to="/gallery">Gallery</Link>
+                    <Link to="/gallery"
+                    onClick={() => setDocTitle("Gallery")}
+                    >Gallery</Link>
                 </li>
             </ul>
         </nav>
